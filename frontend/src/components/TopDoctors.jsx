3073edf1d147c55d8c5aa41506d1ac1417464d5d
John Doe <johndoe@email.com>
2024-11-29 12:33:17 +0000

    commit

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -12,7 +12,7 @@ const TopDoctors = () => {
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 3).map((item, index) => (
+        {doctors.slice(0, 4).map((item, index) => (
           <div
             onClick={() => navigate(`/appointment/${item._id}`)}
             className="border border-secondary rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 "
